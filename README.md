1f73b5569f1b13fe9aba0e5de3265c4dbaf001a7
melpenyogi <melpenyogi2019@gmail.com>
2025-03-22 02:42:26 +0900

    Aplikasi Database Mahasiswa USTJ FIKOM

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # database-mahasiswa
-aplikasi database-mahasiswa 
+
+aplikasi database-mahasiswa
+
+# Aplikasi Database Mahasiswa dengan Login Menggunakan Database dan PHP
+
+Aplikasi Website CRUD sederhana yang digunakan sebagai mendata Data-Data Mahasiswa/i USTJ FIKOM dengan dilengkapi fitur HOME, DATA MAHASISWA dan ABOUT Kemudian Export File, Login dan Logout. Untuk perancangan aplikasi website ini memanfaatkan simple CSS, Database MySQL, dan PHP. Sedangkan untuk pemanggilan alamat website menggunakan server XAMPP.
+
+# Akun Login
+
+- Login Akun untuk login pada website data mahasiswa ini, berikut Username dan Password:
+  Username : admin
+  Password : admin
---
+++ db/mahasiswadb.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 21 Mar 2025 pada 13.18
+-- Versi server: 8.0.30
+-- Versi PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3'),
+(2, 'user', 'ee11cbb19052e40b07aac0ca060c23ee');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nim` varchar(20) NOT NULL,
+  `kelas` varchar(2) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `semester` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nama`, `nim`, `kelas`, `jurusan`, `semester`) VALUES
+(1, 'Iven Lagowan', '18083000200', 'E1', 'Sistem Informasi', '7'),
+(20, 'Meki Gobai', '18083000160', 'E1', 'Sistem Informasi', '14'),
+(21, 'Penias Salawala', '18083000300', 'E1', 'Sistem Informasi', '8');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
